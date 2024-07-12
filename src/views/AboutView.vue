fa2d68c252d985138d81263d436cac3925d92c81
prog31melawai <prog.melawai@gmail.com>
2024-07-12 17:11:10 +0700

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>Ini {{a}}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+let a = 3;
+
+</script>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
